b21669faa8a845920097e584a336593b602e7150
Vitaly Osipov <vi7ali@gmail.com>
2019-05-18 23:49:40 +0300

    Game done

--- web-guesser.rb
+++ web-guesser.rb
@@ -3,6 +3,24 @@ require 'sinatra/reloader' if development?
 
 num = rand(100)+1
 
-get '/' do
-  erb :index, :locals => {:num => num}
+get '/' do  
+  guess = params[:guess].to_i unless params[:guess].nil?
+  message = get_message(guess, num)
+  erb :index, :locals => {num: num, message: message, guess: guess}
+end
+
+def get_message(guess, num)
+  message = ""
+  unless guess.nil?
+    if guess == num
+      message = "Hooray"
+    elsif guess > num
+      message = "Too high!"
+    elsif guess < num
+      message = "Too low!"
+    else
+      message = "Not a num"
+    end
+  end
+  message
 end
